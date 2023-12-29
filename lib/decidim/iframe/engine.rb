a12fef15a834ea1b9331e44e154eddf6f330eace
Joonas <joonas.aapro@mainiotech.fi>
2023-12-29 10:33:40 +0200

    Register capybara driver

--- lib/decidim/iframe/engine.rb
+++ lib/decidim/iframe/engine.rb
@@ -31,6 +31,23 @@ module Decidim
         end
       end
 
+      Capybara.register_driver :headless_chrome do |app|
+        options = ::Selenium::WebDriver::Chrome::Options.new
+        options.args << "--headless=new"
+        options.args << "--no-sandbox"
+        options.args << if ENV["BIG_SCREEN_SIZE"].present?
+                          "--window-size=1920,3000"
+                        else
+                          "--window-size=1920,1080"
+                        end
+        options.args << "--ignore-certificate-errors" if ENV["TEST_SSL"]
+        Capybara::Selenium::Driver.new(
+          app,
+          browser: :chrome,
+          capabilities: [options]
+        )
+      end
+
       def load_seed
         nil
       end
