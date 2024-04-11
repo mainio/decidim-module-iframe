086af549a580fdeb828fcd2cba02bc1ae722e624
Joonas <joonas.aapro@mainiotech.fi>
2024-04-11 16:35:18 +0300

    Fix headless_chrome

--- lib/decidim/iframe/test/rspec_support/capybara.rb
+++ lib/decidim/iframe/test/rspec_support/capybara.rb
@@ -10,7 +10,7 @@ module Decidim
   Capybara.register_driver :headless_chrome do |app|
     options = Selenium::WebDriver::Chrome::Options.new
 
-    options.add_argument("--headless") if ENV["HEADLESS"]
+    options.add_argument("--headless=new") if ENV["HEADLESS"]
     options.add_argument("--no-sandbox")
 
     window_size = ENV["BIG_SCREEN_SIZE"].present? ? "1920,3000" : "1920,1080"
