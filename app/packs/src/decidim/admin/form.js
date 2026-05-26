f7fd0cfb96a4278906df62520cab831ea61c3cac
Joonas <joonas.aapro@mainiotech.fi>
2026-05-26 09:40:03 +0300

    Update form.js

--- app/packs/src/decidim/admin/form.js
+++ app/packs/src/decidim/admin/form.js
@@ -1,4 +1,3 @@
-import "src/decidim/admin/scope_picker_enabler.component"
 import "src/decidim/admin/proposal_infinite_edit"
 import "src/decidim/admin/content_height_toggler"
 import "src/decidim/admin/content_width_toggler"
@@ -7,6 +6,8 @@ import "src/decidim/admin/content_width_toggler"
 
 import BudgetRuleTogglerComponent from "src/decidim/admin/budget_rule_toggler.component"
 
+// Checks if the form contains fields with special CSS classes added in
+// Decidim::Admin::SettingsHelper and acts accordingly.
 $(() => {
   const budgetRuleToggler = new BudgetRuleTogglerComponent({
     ruleCheckboxes: $("input[id^='component_settings_vote_rule_']")
@@ -14,6 +15,7 @@ $(() => {
 
   budgetRuleToggler.run();
 
+  // Prevents readonly containers from being modified.
   const $readonlyContainer = $(".readonly_container input");
 
   $readonlyContainer.click((event) => {
@@ -21,6 +23,15 @@ $(() => {
     return false;
   });
 
+  // Target fields:
+  // - amendments_wizard_help_text
+  // - amendments_visibility
+  // - amendment_creation_enabled
+  // - amendment_reaction_enabled
+  // - amendment_promotion_enabled
+
+  // (1) Hides target fields if amendments_enabled component setting is NOT checked.
+  // (2) Toggles visibility of target fields when amendments_enabled component setting is clicked.
   const $amendmentsEnabled = $("input#component_settings_amendments_enabled");
 
   if ($amendmentsEnabled.length > 0) {
@@ -34,4 +45,21 @@ $(() => {
       $amendmentStepSettings.toggle();
     });
   }
+
+  // Toggles visibility of the attachments_allowed help text when the checkbox is clicked.
+  // Show the help text if the checkbox is checked.
+  const $attachmentsAllowedCheckbox = $("#component_settings_attachments_allowed");
+  const $attachmentsHelpText = $(".attachments_allowed_container .help-text");
+
+  if (!$attachmentsAllowedCheckbox.is(":checked")) {
+    $attachmentsHelpText.hide();
+  }
+
+  $attachmentsAllowedCheckbox.on("change", () => {
+    if ($attachmentsAllowedCheckbox.is(":checked")) {
+      $attachmentsHelpText.show();
+    } else {
+      $attachmentsHelpText.hide();
+    }
+  });
 });
