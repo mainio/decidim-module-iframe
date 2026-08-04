0a7485040ec1ad4b89c0887ce352b9b52a53418b
Joonas <joonas.aapro@mainiotech.fi>
2026-08-04 16:37:12 +0300

    Upgrade to version 0.31

--- lib/decidim/iframe/version.rb
+++ lib/decidim/iframe/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-iframe version.
   module Iframe
     def self.version
-      "0.30.0"
+      "0.31.0"
     end
 
     def self.decidim_version
-      "~> 0.30.0"
+      "~> 0.31.0"
     end
   end
 end
